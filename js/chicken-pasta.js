6e95ece66c7d2678e1cc2d2e388ac8766288d744
Sathish Pittala <48255299+sathishbaburaj@users.noreply.github.com>
2019-07-15 18:06:56 -0400

    added table for nutrition facts, organized javascript to js files, html to html files and css to css files and placed them in respective folder

---
+++ js/chicken-pasta.js
@@ -0,0 +1,49 @@
+var APIKey = "6166d06636850c987ca6129da7d7625f";
+        var APPID = "fc22aa95";
+        var foodURL = "https://trackapi.nutritionix.com/v2/search/instant?query=BBQ chicken";
+        var nutritionURL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
+
+        $.ajax({
+            url: foodURL,
+            method: "GET",
+            headers: {
+                "x-app-id": APPID,
+                "x-app-key": "6166d06636850c987ca6129da7d7625f"
+            },
+        }).then(function (foodsResponse) {
+            console.log(foodsResponse);
+            $.ajax({
+                url: nutritionURL,
+                method: "POST",
+                headers: {
+                    "x-app-id": APPID,
+                    "x-app-key": "6166d06636850c987ca6129da7d7625f"
+                },
+                data: jQuery.param({ "query": "chicken pasta" })
+            }).then(function (nutritionResponse) {
+
+                $(".nutrition").html("<h3>" + 'Chicken Pasta' + " Nutrition Facts</h3>");
+                $(".r1col1").append("calories:");
+                $(".r1col2").append(nutritionResponse.foods[0].nf_calories + '%'+'<br>')
+                $(".r2col1").append("cholesterol:");
+                $(".r2col2").append(nutritionResponse.foods[0].nf_cholesterol + '%'+'<br>')
+                $(".r3col1").append("potassium:");
+                $(".r3col2").append(nutritionResponse.foods[0].nf_potassium + '%'+'<br>')
+                $(".r4col1").append("protein:");
+                $(".r4col2").append(nutritionResponse.foods[0].nf_protein + '%'+'<br>')
+                $(".r5col1").append("saturated fat:");
+                $(".r5col2").append(nutritionResponse.foods[0].nf_saturated_fat + '%'+'<br>')
+                $(".r6col1").append("sodium:");
+                $(".r6col2").append(nutritionResponse.foods[0].nf_sodium + '%'+'<br>')
+                $(".r7col1").append("sugar:");
+                $(".r7col2").append(nutritionResponse.foods[0].nf_sugars + '%'+'<br>')
+                $(".r8col1").append("total carbohydrate:");
+                $(".r8col2").append(nutritionResponse.foods[0].nf_total_carbohydrate + '%'+'<br>')
+                $(".r9col1").append("total fat:");
+                $(".r9col2").append(nutritionResponse.foods[0].nf_total_fat + '%'+'<br>')
+
+
+
+
+            });
+        });
